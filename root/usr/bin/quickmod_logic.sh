8d2d1b5e946d85901afe4b124f7c418942eb0147
jhonamazingapple <103912606+jhonamazingapple@users.noreply.github.com>
2026-03-14 22:20:17 +0800

    Update quickmod_logic.sh

--- root/usr/bin/quickmod_logic.sh
+++ root/usr/bin/quickmod_logic.sh
@@ -5,19 +5,20 @@ ACTION="$1"
 random_hostname() {
 	HEX="$(hexdump -n 3 -e '3/1 "%02X"' /dev/urandom 2>/dev/null)"
 	[ -n "$HEX" ] || HEX="$(date +%s | tail -c 7)"
-	uci set system.@system[0].hostname="OpenWrt-$HEX"
+	NEW_HOSTNAME="OpenWrt-$HEX"
+	uci set system.@system[0].hostname="$NEW_HOSTNAME"
 }
 
 random_mac() {
-	# 生成本地管理单播 MAC
 	B1="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
 	B2="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
 	B3="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
 	B4="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
 	B5="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
+	B6="$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
 
 	FIRST="$(printf '%02X' $(( (0x$B1 | 0x02) & 0xFE )))"
-	NEW_MAC="$FIRST:$B2:$B3:$B4:$B5:$(hexdump -n 1 -e '1/1 "%02X"' /dev/urandom 2>/dev/null)"
+	NEW_MAC="$FIRST:$B2:$B3:$B4:$B5:$B6"
 
 	uci set network.lan.macaddr="$NEW_MAC"
 }
@@ -27,7 +28,7 @@ if [ "$ACTION" = "random" ]; then
 	random_mac
 	uci commit system
 	uci commit network
-	echo '{"result":"ok"}'
+	echo "{\"result\":\"ok\",\"hostname\":\"$NEW_HOSTNAME\",\"macaddr\":\"$NEW_MAC\"}"
 	exit 0
 fi
 
